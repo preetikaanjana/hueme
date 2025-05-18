dbb39b8fcc0e84520d5bc757cfd918635c6ec32d
Preetika Anjana <preetikaanjana@gmail.com>
2025-05-18 21:30:34 +0530

    Fix Vite base path for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
- 
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-  base: '/hueme/',
-}) 
+  base: '/', // ← FIXED
+});
